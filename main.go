8dcf6a1130bd7331e0e41ff41e6011cf65e63d08
christophe dhayer <christophe.dhayer@gmail.com>
2020-06-28 17:20:29 -0400

    add basic server

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
 func main() {
-	fmt.Println("this a test"
+	http.HandleFunc("/", hdlFunc)
+	port := getPORT()
+	fmt.Println("serve on ", port)
+	http.ListenAndServe(getPORT(), nil)
+}
+
+func hdlFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "<h1>hello</h1>")
+}
+
+func getPORT() string {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = ":8080"
+	}
+
+	return port
 }
