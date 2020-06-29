b43070894c523fbe6b1f582c2e9a0855cc1af57e
christophe dhayer <christophe.dhayer@gmail.com>
2020-06-29 18:14:46 -0400

    add router

--- main.go
+++ main.go
@@ -8,14 +8,38 @@ import (
 
 func main() {
 	http.HandleFunc("/", hdlFunc)
+	http.HandleFunc("/contact", contact)
 	port := getPORT()
 	fmt.Println("serve on ", port)
 	http.ListenAndServe(getPORT(), nil)
 }
 
 func hdlFunc(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
+	path := r.URL.Path
+	fmt.Println(path)
+	switch path {
+	case "contact":
+		contact(w, r)
+	case "/":
+		root(w, r)
+	default:
+		p404(w, r)
+	}
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte(`<h1>Contact</h1>  
+		<p><a href="mailto:christophe.dhayer@gmail.com">Mail to me</a></p>
+	`))
+}
+
+func root(w http.ResponseWriter, r *http.Request) {
+	// w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>hello you how are you</h1>")
+}
+
+func p404(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, `<h1>error 404 page not found</h1>`)
 }
 
 func getPORT() string {
